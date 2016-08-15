1614aee16f9475950d4d20d853847c848ce68c69
Jeffrey Meyer <itotallyrock@users.noreply.github.com>
2016-08-15 14:25:13 -0500

    Add tests for hyphen escaping

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ var xkcd37 = require('./')
 var t = require('assert')
 
 t.equal(xkcd37('Man, that\'s a sweet-ass car'), 'Man, that\'s a sweet ass-car')
+t.equal(xkcd37('Man, that\'s a sweet\-ass car'), 'Man, that\'s a sweet-ass car')
 
 t.throws(function () {
   xkcd37(1337)
